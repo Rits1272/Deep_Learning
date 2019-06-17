bd81551d5daf2f1a861c7736303ac9032ae6967d
Ritik Jain <ritikjain1272@gmail.com>
2019-06-17 15:34:28 +0530

    Update digit_classifier.py.py

--- digit_classifier.py.py
+++ digit_classifier.py.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[19]:
-
-
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 mnist = tf.keras.datasets.mnist  # 28x28 images of handwritten digits 0-9 28 rows and 28 columns
 
@@ -16,7 +13,7 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 model = tf.keras.models.Sequential() # Most common model
 
 # Whenever you need to convert a multidimensional 
-# tensor into a single 1-D tensor, use can use Flatten.
+# tensor into a single 1-D tensor, we can use Flatten.
 
 # Input Layer
 model.add(tf.keras.layers.Flatten())
@@ -33,44 +30,15 @@ model.compile(optimizer='adam',
 
 model.fit(x_train, y_train, epochs=3)
 
-
-# In[20]:
-
-
 val_loss, val_accuracy = model.evaluate(x_test, y_test)
 print(val_loss)
 print(val_accuracy)
 
-
-# In[23]:
-
-
 model.save('epic_num_reader.model')
 new_model = tf.keras.models.load_model('epic_num_reader.model')
-
-
-# In[26]:
-
-
 predictions = new_model.predict(x_test)
-
-
-# In[35]:
-
 
 print(np.argmax(predictions[0]))
 
-
-# In[36]:
-
-
-import matplotlib.pyplot as plt
 plt.imshow(x_test[0])
 plt.show()
-
-
-# In[ ]:
-
-
-
-
